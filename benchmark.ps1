285c254647b0e3748da84f9d640f59804128b562
martincostello <martin@martincostello.com>
2024-08-20 13:20:54 +0100

    Remove `-Filter` parameter Remove the `-Filter` parameter and just pass a literal `*` as otherwise PowerShell keeps trying to expand it, breaking the invocation.

--- benchmark.ps1
+++ benchmark.ps1
@@ -4,7 +4,6 @@
 #Requires -Version 7
 
 param(
-    [Parameter(Mandatory = $false)][string] $Filter = "",
     [Parameter(Mandatory = $false)][string] $Framework = "net8.0",
     [Parameter(Mandatory = $false)][string] $Job = ""
 )
@@ -74,10 +73,7 @@ $benchmarks = (Join-Path $solutionPath "src" "DotNetBenchmarks" "DotNetBenchmark
 
 Write-Host "Running benchmarks..." -ForegroundColor Green
 
-$additionalArgs = @(
-    "--filter",
-    [string]::IsNullOrEmpty($Filter) ? '`*' : $Filter
-)
+$additionalArgs = @()
 
 if (-Not [string]::IsNullOrEmpty($Job)) {
     $additionalArgs += "--job"
@@ -89,4 +85,4 @@ if (-Not [string]::IsNullOrEmpty(${env:GITHUB_SHA})) {
     $additionalArgs += "json"
 }
 
-& $dotnet run --project $benchmarks --configuration "Release" --framework $Framework -- $additionalArgs
+& $dotnet run --project $benchmarks --configuration "Release" --framework $Framework -- $additionalArgs --% --filter *
